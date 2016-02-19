624d9b51713a841274cbf115d9d8bc4d9fab1c5e
bramoosterhuis <bram.oosterhuis.nl@gmail.com>
2016-02-20 00:11:16 +0100

    Add v21 init script

--- package/intelce-sdk/intelce-loaddrivers/intelce-loaddrivers.mk
+++ package/intelce-sdk/intelce-loaddrivers/intelce-loaddrivers.mk
@@ -11,7 +11,7 @@ INTELCE_LOADDRIVERS_REDISTRIBUTE = NO
 INTELCE_LOADDRIVERS_DEPENDENCIES = intelce-sdk
 
 define INTELCE_LOADDRIVERS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 755 package/intelce-sdk/intelce-loaddrivers/init $(TARGET_DIR)/etc/init.d/S20loaddrivers
+	$(INSTALL) -m 755 package/intelce-sdk/intelce-loaddrivers/init_${INTELCE_SDK_RELEASE} $(TARGET_DIR)/etc/init.d/S20loaddrivers
 	$(INSTALL) -m 755 package/intelce-sdk/intelce-loaddrivers/init_utilities $(TARGET_DIR)/etc/init.d/init_utilities
 endef
 
