c5adce564644cb0ac23f6e9e7898ab55b07c4643
bramoosterhuis <bram.oosterhuis.nl@gmail.com>
2016-02-20 01:28:35 +0100

    Fix install library

--- package/intelce-sdk/package/intelce-hdmi_hdcp/intelce-hdmi_hdcp.mk
+++ package/intelce-sdk/package/intelce-hdmi_hdcp/intelce-hdmi_hdcp.mk
@@ -37,10 +37,12 @@ endef
 
 define INTELCE_HDMI_HDCP_INSTALL_STAGING_CMDS
     $(call INTELCE_SDK_INSTALL_TO_STAGING,${INTELCE_HDMI_HDCP_DIR}/build_i686/staging_dir) 
+	$(INSTALL) -m 644 ${INTELCE_HDMI_HDCP_DIR}/build_i686/staging_dir/lib/libsec_hdcp.so $(STAGING_DIR)/lib/libsec_hdcp.so
 endef
 
 define INTELCE_HDMI_HDCP_INSTALL_TARGET_CMDS
     $(call INTELCE_SDK_INSTALL_TO_TARGET,${INTELCE_HDMI_HDCP_DIR}/project_build_i686/IntelCE/root)
+	$(INSTALL) -m 644 ${INTELCE_HDMI_HDCP_DIR}/build_i686/staging_dir/lib/libsec_hdcp.so $(TARGET_DIR)/lib/libsec_hdcp.so
 endef
 
 $(eval $(generic-package))
