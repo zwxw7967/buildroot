c9cdf47aab695785d43e24baff5eef45d8850936
Philippe Normand <philn@igalia.com>
2016-11-24 16:13:05 +0100

    libnice: bump and add a few patches

--- package/libnice/libnice.mk
+++ package/libnice/libnice.mk
@@ -4,13 +4,21 @@
 #
 ################################################################################
 
-LIBNICE_VERSION = 0.1.13
-LIBNICE_SITE = http://nice.freedesktop.org/releases
+LIBNICE_VERSION = 2803a0b4b70af9684e05ef5ed3f0c2fbca4b6c93
+LIBNICE_SITE = http://cgit.freedesktop.org/libnice/libnice/snapshot
+BR_NO_CHECK_HASH_FOR += $(LIBNICE_SOURCE)
 LIBNICE_LICENSE = MPLv1.1 or LGPLv2.1
 LIBNICE_LICENSE_FILES = COPYING COPYING.MPL COPYING.LGPL
 LIBNICE_DEPENDENCIES = libglib2 host-pkgconf
 LIBNICE_INSTALL_STAGING = YES
 LIBNICE_AUTORECONF = YES
+
+define LIBNICE_RUN_AUTOGEN
+        cd $(@D) && PATH=$(BR_PATH) NOCONFIGURE=1 ./autogen.sh
+endef
+LIBNICE_PRE_CONFIGURE_HOOKS += LIBNICE_RUN_AUTOGEN
+
+LIBNICE_CONF_OPTS += --enable-compile-warnings=no
 
 ifeq ($(BR2_PACKAGE_GSTREAMER1),)
 LIBNICE_CONF_OPTS += --without-gstreamer
